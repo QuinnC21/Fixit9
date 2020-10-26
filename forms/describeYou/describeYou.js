8bf86ac3bd21c342014f17d5d1af2bb8bbf90836
QuinnC21 <ccquinn99@gmail.com>
2020-10-25 21:22:43 -0500

    Hw

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,4 +1,10 @@
+radYou.onchange=function(){
+  lblOut.value = `I agree you are a very ${$("input[name=radYou]:checked").prop("value")} person!`
+}
 
+btnNextII.onclick=function(){
+  ChangeForm(favExercises)
+}
 
 
 
--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -2,11 +2,27 @@ var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
 
 favExercises.onshow=function(){
-
+  for (i = 0; i<= exercises.length-1; i++) {
+   selExercises.addItem(exercises[i])
+   }
+for (i = 0;i<=coreExercises.length-1;i++) {
+  selExercises.addItem(coreExercises[i])
+  }
 }
 
 selExercises.onfocusout=function(){
-
 }
 
+btnSubmit.onclick=function() {
+  if (selExercises.text[0]=="situps" && selExercises.text[1]=="plank")  {
+  lblMessage.value = `You chose ${selExercises.text[0]} and ${selExercises.text[1]} - those are the two core exercises.`
+  }
+  else {  
+  lblMessage.value = "You did not pick the two core exercises"
+  }
+}
+
+  btnNextIII.onclick=function(){
+  ChangeForm(mobileNav)
+}
 
